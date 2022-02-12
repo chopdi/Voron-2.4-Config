61fa565f165a6f3f3aa1058a42407871e9604fde
chopdi <choopdiup@googlemail.com>
2022-02-12 22:27:22 +0100

    Backup triggered on 2022-02-12 22:27:22
    Klipper on commit: e3cbe7e
    
    Moonraker on commit: 92eda98
    
    Mainsail version: v2.1.1

--- gitbackup.sh
+++ gitbackup.sh
@@ -67,7 +67,7 @@ push_config(){
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Backup triggered on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
   #git push "https://Voron 2.4 Config:$GH_TOKEN@github.com/chopdi/Voron-2.4-Config"
-  git push "https://github.com/chopdi/Voron-2.4-Config.git"
+  git push "git@github.com:chopdi/Voron-2.4-Config.git"
  
 }
 
