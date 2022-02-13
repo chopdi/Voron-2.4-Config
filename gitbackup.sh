ba08eb120e4f765abfc30a4e341ef0c478f9505b
chopdi <choopdiup@googlemail.com>
2022-02-13 09:51:49 +0100

    autocommit from 2022-02-13 09:51:49
    Klipper on commit: e3cbe7e
    
    Moonraker on commit: 92eda98
    
    Mainsail version: v2.1.1

--- gitbackup.sh
+++ gitbackup.sh
@@ -66,7 +66,7 @@ push_config(){
   git add . -v
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-   git push "https://chopdi:$GH_TOKEN@github.com/chopdi/Voron-2.4-Config.git" 
+  git push "https://chopdi:$GH_TOKEN@github.com/chopdi/Voron-2.4-Config.git" 
 #  git push
 }
 
