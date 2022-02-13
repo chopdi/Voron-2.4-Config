204495dc93face2716689794a3e55dd08fb01e18
chopdi <choopdiup@googlemail.com>
2022-02-13 09:52:42 +0100

    autocommit from 2022-02-13 09:52:42
    Klipper on commit: e3cbe7e
    
    Moonraker on commit: 92eda98
    
    Mainsail version: v2.1.1

--- gitbackup.sh
+++ gitbackup.sh
@@ -78,10 +78,10 @@ VERSION=`git describe --abbrev=0 --tags`
 #VERSION_BITS=(${VERSION//./ })
 
 #get number parts and increase last one by 1
-VNUM1=${VERSION_BITS[0]}
-VNUM2=${VERSION_BITS[1]}
-VNUM3=${VERSION_BITS[2]}
-VNUM3=$((VNUM3+1))
+#VNUM1=${VERSION_BITS[0]}
+#VNUM2=${VERSION_BITS[1]}
+#VNUM3=${VERSION_BITS[2]}
+#VNUM3=$((VNUM3+1))
 
 #create new tag
 NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
