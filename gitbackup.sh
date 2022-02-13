0e42501f0054a701d8b01c20acc3a4cda0d4e2ad
chopdi <choopdiup@googlemail.com>
2022-02-13 09:50:51 +0100

    Ratrig-1 autocommit from 2022-02-13 09:50:51
    Klipper on commit: e3cbe7e
    
    Moonraker on commit: 92eda98
    
    Mainsail version: v2.1.1

--- gitbackup.sh
+++ gitbackup.sh
@@ -65,11 +65,39 @@ push_config(){
   git pull -v
   git add . -v
   current_date=$(date +"%Y-%m-%d %T")
-  git commit -m "Backup triggered on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git push "https://chopdi:$GH_TOKEN@github.com/chopdi/Voron-2.4-Config.git" --tags
-  #git push "https://github.com/chopdi/Voron-2.4-Config.git"
- 
+  git commit -m "Ratrig-1 autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+   git push "https://chopdi:$GH_TOKEN@github.com/chopdi/Voron-2.4-Config.git" 
+#  git push
 }
 
 grab_version
 push_config
+#get highest tag number
+VERSION=`git describe --abbrev=0 --tags`
+
+#replace . with space so can split into an array
+VERSION_BITS=(${VERSION//./ })
+
+#get number parts and increase last one by 1
+VNUM1=${VERSION_BITS[0]}
+VNUM2=${VERSION_BITS[1]}
+VNUM3=${VERSION_BITS[2]}
+VNUM3=$((VNUM3+1))
+
+#create new tag
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+
+echo "Updating $VERSION to $NEW_TAG"
+
+#get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+
+#only tag if no tag already
+if [ -z "$NEEDS_TAG" ]; then
+    git tag $NEW_TAG
+    echo "Tagged with $NEW_TAG"
+     git push "https://chopdi:$GH_TOKEN@github.com/chopdi/Voron-2.4-Config.git" --tags
+else
+    echo "Already a tag on this commit"
+fi
